9604d733dbf3658cff984a82f2b9116a9f04e0b1
tzhang417 <tzhang417@gatech.edu>
2023-09-13 21:52:21 -0400

    Update vcpu_scheduler.c

--- cpu/vcpu_scheduler.c
+++ cpu/vcpu_scheduler.c
@@ -9,14 +9,14 @@ int main(int argc, char *argv[])
     if (argc < 2)
     {
         printf("Invalid Command\n");
-        return -1
+        return -1;
     }
 
     int interval = atoi(argv[1]);
     if (sleep <= 0) 
     {
         printf("Invalid Interval\n");
-        return -1
+        return -1;
     }
 
     conn = virConnectOpen("qemu:///system");
@@ -30,6 +30,7 @@ int main(int argc, char *argv[])
     printf("Number of pCpus: %d\n", pCpu);
 
     int numDomains = virConnectListAllDomains(conn, &domains, VIR_CONNECT_LIST_DOMAINS_ACTIVE);
+    int curP = 0
     for (int i = 0, i < numDomains; i ++)
     {
         curP %= pCpu;
