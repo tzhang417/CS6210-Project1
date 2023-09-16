15b5df3f4c9b732b89820744c5a25bbbde4c9500
tzhang417 <tzhang417@gatech.edu>
2023-09-16 16:37:38 -0400

    reformat

--- cpu/vcpu_scheduler.c
+++ cpu/vcpu_scheduler.c
@@ -1,33 +1,70 @@
-#include <float.h>
-#include <limits.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <libvirt/libvirt.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<libvirt/libvirt.h>
+#include<math.h>
+#include<string.h>
+#include<unistd.h>
+#include<limits.h>
+#include<signal.h>
+#define MIN(a,b) ((a)<(b)?a:b)
+#define MAX(a,b) ((a)>(b)?a:b)
 
+int is_exit = 0; // DO NOT MODIFY THIS VARIABLE
+
+
+void CPUScheduler(virConnectPtr conn,int interval);
+
+/*
+DO NOT CHANGE THE FOLLOWING FUNCTION
+*/
+void signal_callback_handler()
+{
+	printf("Caught Signal");
+	is_exit = 1;
+}
+
+/*
+DO NOT CHANGE THE FOLLOWING FUNCTION
+*/
 int main(int argc, char *argv[])
 {
+	virConnectPtr conn;
+
+	if(argc != 2)
+	{
+		printf("Incorrect number of arguments\n");
+		return 0;
+	}
+
+	// Gets the interval passes as a command line argument and sets it as the STATS_PERIOD for collection of balloon memory statistics of the domains
+	int interval = atoi(argv[1]);
+	
+	conn = virConnectOpen("qemu:///system");
+	if(conn == NULL)
+	{
+		fprintf(stderr, "Failed to open connection\n");
+		return 1;
+	}
+
+	// Get the total number of pCpus in the host
+	signal(SIGINT, signal_callback_handler);
+
+	while(!is_exit)
+	// Run the CpuScheduler function that checks the CPU Usage and sets the pin at an interval of "interval" seconds
+	{
+		CPUScheduler(conn, interval);
+		sleep(interval);
+	}
+
+	// Closing the connection
+	virConnectClose(conn);
+	return 0;
+}
+
+void CPUScheduler(virConnectPtr conn, int interval)
+{
     virNodeInfo nodeInfo;
-    virConnectPtr conn;
     virDomainPtr *domains;
-    if (argc < 2)
-    {
-        printf("Invalid Command\n");
-        return -1;
-    }
-
-    int interval = atoi(argv[1]);
-    if (interval <= 0) 
-    {
-        printf("Invalid Interval\n");
-        return -1;
-    }
-
-    conn = virConnectOpen("qemu:///system");
-    if (conn == NULL)
-    {
-        printf("Failed to Connect to the Hypervisor");
-    }
 
     virNodeGetInfo(conn, &nodeInfo);
     int pCpu = (int) nodeInfo.cpus;
