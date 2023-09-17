c291dc63b9d374b5a4aca42c19c4e9ac9b34132a
tzhang417 <tzhang417@gatech.edu>
2023-09-17 19:36:31 -0400

    Update vcpu_scheduler.c

--- cpu/vcpu_scheduler.c
+++ cpu/vcpu_scheduler.c
@@ -112,10 +112,11 @@ void getPercentage(virDomainPtr *domains, int numDomains, double *cpuPercentage,
     }
     
     sleep(interval);
+
     for (int i = 0; i < numDomains; i++)
     {
         virDomainGetCPUStats(domains[i], params, nparams, -1 ,1, 0);
-        printf("%lld", params[0].value.l - prevCpuTime[i]);
+        printf("%lld", params[0].value.l);
         cpuPercentage[domainToCpu[i]] += 100.0 * (params[0].value.l - prevCpuTime[i])/(interval * 1000000000);
     }
 }
